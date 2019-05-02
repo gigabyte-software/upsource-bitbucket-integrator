96d2acf2c498c8106c02b88140f582e4f9d82803
Chris Chambers <chris@gigabyte.software>
2019-05-02 10:02:35 +0100

    Working PUT request to Bitbucket with title data from get request

--- src/public/index.php
+++ src/public/index.php
@@ -16,6 +16,73 @@ $app = new \Slim\App([
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+// Create GET request from Slim (Chrome) to Apache, searching for URL (with /bucket) in chrome triggers
+// slim app to run this method (function?)
+$app->get('/bucket', function (Request $request, Response $response) {
+
+    // I need a guzzle client, which is an http client that I can make requests with (like Chrome)
+    $guzzleHttpClient = new Client();
+    $guzzleResponse = $guzzleHttpClient->request("GET",'https://api.bitbucket.org/2.0/repositories/gigabyte-software/review-creator/pullrequests',
+        ['auth' => [
+            'rowBawTick',
+            'fr%XUtC7git'
+        ]]);
+    $pullRequest = $guzzleResponse->getBody();
+    $response->getBody()->write($pullRequest);
+
+    return $response;
+});
+
+// Create GET request from Slim (Chrome) to Apache, searching for URL (with /putbucket/1) in chrome triggers
+// slim app to run this method (function?) - simulated id in URL - todo get id before this and then append it onto URL
+$app->get('/putbucket/{id}', function (Request $request, Response $slimResponse, $args) {
+
+    // I need a guzzle client, which is an http client that I can make requests with (like Chrome)
+    $guzzleHttpClient = new Client();
+    // First I need to get pull request data
+    $guzzleResponse = $guzzleHttpClient->request("GET",'https://api.bitbucket.org/2.0/repositories/gigabyte-software/review-creator/pullrequests',
+        ['auth' => [
+            'rowBawTick',
+            'fr%XUtC7git'
+        ]]
+    );
+
+    // Getting contents of body from guzzleResponse (which is a long line of json text)
+    $pullRequestBody = $guzzleResponse->getBody()->getContents();
+    // decode body of guzzle response (pullRequest) into an array, assoc (array) = true
+    $pullRequestArray = json_decode($pullRequestBody, true);
+
+    //    Debugging...
+    //    $debug = print_r($pullRequestArray['pagelen'], true);
+    $debug = print_r($pullRequestArray['values'][0]['title'], true);
+//    echo '<pre>',print_r($pullRequestBody,1),'</pre>';
+
+    // Create PUT request from guzzleResponse, pass in method (required), uri and an array (can be array of arrays -
+    // auth and json are preset acceptable arrays)
+    $guzzleResponse = $guzzleHttpClient->request(
+        "PUT",
+        'https://api.bitbucket.org/2.0/repositories/gigabyte-software/review-creator/pullrequests/' . $args['id'],
+        [
+            'auth' => [
+                'rowBawTick',
+                'fr%XUtC7git',
+            ],
+            'json' => [
+                'id' => 1,
+                'title' => 'Test2',
+//                'title' => $pullRequestArray['values'][0]['title'],
+                'description' => "Test2 put request"
+            ]
+        ]
+    );
+
+    // Write $debug to body of slim response so I can see it in the browser
+    $slimResponse->getBody()->write($debug);
+
+    return $slimResponse;
+});
+
+
 // Create a POST request for a message to BitBucket
 $app->get('/bitbucket', function (Request $request, Response $response) {
 
@@ -34,23 +101,6 @@ $app->get('/bitbucket', function (Request $request, Response $response) {
 
     return $response;
 });
-
-// Create a POST request for a message to BitBucket
-$app->get('/bucket', function (Request $request, Response $response) {
-
-    // I need a guzzle client, which is an http client that I can make requests with (like Chrome)
-    $guzzleHttpClient = new Client();
-    $guzzleResponse = $guzzleHttpClient->request("GET",'https://api.bitbucket.org/2.0/repositories/gigabyte-software/review-creator/pullrequests',
-        ['auth' => [
-            'rowBawTick',
-            'fr%XUtC7git'
-        ]]);
-    $pullRequest = $guzzleResponse->getBody();
-    $response->getBody()->write($pullRequest);
-
-    return $response;
-});
-
 
 // app is dealing with a get request of the format http://dev.review-creator/hello/chris/chambers
 // Taking $args from {}, setting $request variable and instantiating $response variable
