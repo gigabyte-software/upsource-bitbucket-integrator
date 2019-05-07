0e547c421ca0cfb04a9b305c027250c7e9694ca5
Chris Chambers <chris@gigabyte.software>
2019-05-07 17:50:31 +0100

    Created container to instantiate bibucketService, passed in username and password then fetched container in HookController

--- public/index.php
+++ public/index.php
@@ -1,19 +1,40 @@
 <?php
 
+use Services\BitbucketService;
+
 require_once '../vendor/autoload.php';
 
-$app = new \Slim\App([
-    'settings' => [
-        'displayErrorDetails' => true,
-    ],
-]);
-
 // Load environment variables (for user and pass) from .env file
-$dotenv = Dotenv\Dotenv::create(__DIR__."/..");
+$dotenv = Dotenv\Dotenv::create(__DIR__ . "/..");
 $dotenv->load();
 
-// Define route
-// Searching for URL (with dev.review-creator/bucket) in chrome (slim) triggers GET request to Apache
+// Instantiate Slim container and set debugging/error settings
+// Dependency container instance is injected into the Slim app's constructor???
+$container = new \Slim\Container([
+    'settings' => [
+        'displayErrorDetails' => true,
+        'determineRouteBeforeAppMiddleware' => true,
+        'debug' => true],
+]);
+
+$app = new \Slim\App($container);
+
+// Retrieving container (not sure why this is needed)
+$container = $app->getContainer();
+// Add bitbucketService to Slim container
+$container['bitbucketService'] = function ($container) {
+
+    // Get username and password from .env file
+    $username = getenv('BITBUCKET_USERNAME');
+    $password = getenv('BITBUCKET_PASSWORD');
+
+    // Instantiate object from class BitBucketService() and pass in username and password
+    $bitbucketService = new BitbucketService($username, $password);
+
+    return $bitbucketService;
+};
+
+// Define route - searching for URL (with dev.review-creator/bucket) in chrome (slim) triggers GET request to Apache
 // (dev.review-creator points to review creator from config file in vhost (vagrant). Slim app then runs HookController
 $app->get('/bitbucket/{id}', '\Controllers\HookController:index');
 
