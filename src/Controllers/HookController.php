d7977b977693eae504fc8eaac074e3afaacce4bf
Jakub Giminski <jakubgiminski@gmail.com>
2019-06-16 20:03:24 +0100

    Clean up

--- src/Controllers/HookController.php
+++ src/Controllers/HookController.php
@@ -46,42 +46,6 @@ class HookController
      * and return URL to Bitbucket
      * @param Request $request
      * @return Response
-     */
-    public function createUpsourceReview(Request $request): Response
-    {
-        // Get contents of body from Bitbucket POST request (webhook) and decode
-        $requestBody = $request->getBody()->getContents();
-        $bitbucketPullRequest = json_decode($requestBody, true);
-
-        $this->logger->addDebug('HookController:createReview Webhook retrieved ' . $requestBody);
-
-        // Extract full/repo name, pullRequestId and branch name (upsource-api-integration) from Bitbucket webhook
-        $bitbucketRepositoryFullName = $bitbucketPullRequest['repository']['full_name'];
-        $bitbucketRepositoryName = $bitbucketPullRequest['pullrequest']['source']['repository']['name'];
-        $bitbucketPullRequestId = $bitbucketPullRequest['pullrequest']['id'];
-        $bitbucketBranchName = $bitbucketPullRequest['pullrequest']['source']['branch']['name'];
-
-        /** @var UpsourceService $upsourceService */
-        // Create Upsource Review and pass in branchName amd repositoryName from Bitbucket webhook
-        $upsourceReviewUrl = $this->upsourceService->createReview(
-            $bitbucketRepositoryName,
-            $bitbucketBranchName
-        );
-
-        // Update Bitbucket description with upsource url (pass in bitbucket's full repo name, pullRequestId - also
-        // need title and current description to append but these are retrieved in BitbucketService)
-        $this->bitbucketService->changePullRequestDescription($bitbucketRepositoryFullName, $bitbucketPullRequestId,
-            $upsourceReviewUrl);
-
-        // Return response once all logic in app is completed, upsource review has been created and retrieved link
-        return new \Slim\Http\Response();
-    }
-
-    /**
-     * Take webhook POST request from Bitbucket and make a POST request to createReview in an UpsourceService
-     * and return URL to Bitbucket
-     * @param Request $request
-     * @return Response
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function createUpsourceReviewWithModel(Request $request): Response
